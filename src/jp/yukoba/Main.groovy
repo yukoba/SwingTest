28e0c56b7c7db85cbad1c7d0c7ca5a1e4cef5a0b
Yu Kobayashi <yukoba@accelart.jp>
2014-07-20 17:37:48 +0900

    GriffonTestを普通のSwingに移植してみた

---
+++ src/jp/yukoba/Main.groovy
@@ -0,0 +1,62 @@
+package jp.yukoba
+
+import groovy.transform.TypeChecked
+
+import javax.swing.JFrame
+import javax.swing.UIManager
+import javax.swing.event.TreeSelectionEvent
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeModel
+import java.awt.event.ActionEvent
+
+@TypeChecked
+class Main {
+    String count = "0"
+
+    final MainForm mainForm = new MainForm()
+    final DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("追加した数")
+    final DefaultTreeModel treeModel = new DefaultTreeModel(rootNode)
+
+    Main() {
+        mainForm.incrementButton.addActionListener { ActionEvent evt -> onIncrementButton() }
+
+        mainForm.numberTree.model = treeModel
+        mainForm.numberTree.addTreeSelectionListener { TreeSelectionEvent evt -> onTreeClick(evt) }
+
+        countUpdated()
+
+        createFrame()
+    }
+
+    void onIncrementButton() {
+        count = mainForm.countTF.text
+        count = ((count as int) + 1) as String
+
+        countUpdated()
+
+        treeModel.insertNodeInto(new DefaultMutableTreeNode(count), rootNode, rootNode.childCount)
+        mainForm.numberTree.expandRow(0)
+    }
+
+    void onTreeClick(TreeSelectionEvent evt) {
+        println "Tree click: ${evt.path.lastPathComponent}"
+    }
+
+    void countUpdated() {
+        mainForm.countTF.text = count
+        mainForm.countLabel.text = count
+    }
+
+    void createFrame() {
+        def frame = new JFrame("MainForm")
+        frame.contentPane = mainForm.mainPanel
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.pack()
+        frame.visible = true
+    }
+
+    static void main(String[] args) {
+        UIManager.setLookAndFeel(UIManager.systemLookAndFeelClassName)
+        new Main()
+    }
+}
---
+++ src/jp/yukoba/MainForm.java
@@ -0,0 +1,11 @@
+package jp.yukoba;
+
+import javax.swing.*;
+
+public class MainForm {
+    public JTextField countTF;
+    public JTree numberTree;
+    public JButton incrementButton;
+    public JLabel countLabel;
+    public JPanel mainPanel;
+}
